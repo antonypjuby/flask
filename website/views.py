7934cf55bfa270923c4dca88f0e35418e2f8791a
Antony <tonypjuby@gmail.com>
2023-03-14 16:16:29 +0530

    second commit

--- website/views.py
+++ website/views.py
@@ -1,6 +1,9 @@
 from flask import Blueprint,render_template
 views = Blueprint('views',__name__)
+from flask_login import  login_required,  current_user
+
 
 @views.route('/')
+@login_required
 def home():
     return render_template("home.html")
